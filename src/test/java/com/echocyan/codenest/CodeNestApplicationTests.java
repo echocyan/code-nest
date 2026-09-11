99d33fed87b4fde791770d975980463306f29f5c
EchoCyan <echo.cyan@outlook.com>
2026-09-11 14:13:20 +0800

    Initial commit

---
+++ src/test/java/com/echocyan/codenest/CodeNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echocyan.codenest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
